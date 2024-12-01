6d66ce2ec104e42586799275fe65f3927d3e2a87
Shibam Mohanty <106046613+SHIBAMMOHANTY@users.noreply.github.com>
2024-12-01 17:38:56 +0530

    fix

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import '../CSS/Header.css';
 import { Link } from 'react-router-dom';
 
-
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -17,12 +16,12 @@ const Header = () => {
       </span>
       <nav className={`menu ${menuOpen ? 'open' : ''}`}>
         <ul>
-          <li><Link to ="/">Home</Link></li>
-          <li><Link to ="about">About Us</Link></li>
-          <li><Link to ="services">Services</Link></li>
-          <li><Link to ="contact">Contact</Link></li>
+          {/* Add leading slash for proper routing */}
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About Us</Link></li>
+          <li><Link to="/services">Services</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
         </ul>
-        <a href="#login" className="login-btn">Log In</a>
       </nav>
       <button 
         className="menu-toggle" 
